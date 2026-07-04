01fcf2cfe684a384da18f2c2ce7e2799b6f65655
yootoshima <yootoshi53258@gmail.com>
2026-07-04 10:54:41 +0700

    Default.vueSuccess

---
+++ backend/middleware/authmiddleware.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken')
+const JWT_SECRET = process.env.JWT_SECRET
+
+export.verifyToken = (req,res,next)=>{
+    const authHeader = req.header("Authotization")
+    if(!authHeader || !authHeader.starsWith("Bearer")) return
+    res.status(401).json({message:'Invalid No or token'})
+    try {
+        req.user = jwt.verify(token,JWT_SECRET)
+        next()
+    } catch (error) {
+        console.error('Invalid Token',error)
+        res.status(403).json({message:'Invalidr token'})
+    }
+}
+
+export.requierRole = (role) =>(req,res,next)=>{
+    req.user?.role === role
+    ? next()
+    : res.status(403).json({message:'Invalidr token'})
+}
---
+++ frontend/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1 class="ml-6">test</h1>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
